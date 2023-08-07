e2b35703836c40da8c1ebdadaa4a087a9fe881c3
Adelmo Junior <adelmojr07@gmail.com>
2023-08-07 20:09:31 -0300

    chore: love message

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 function Home() {
-    return <h1>Hello World, in vercel!</h1>
+    return <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column'}}>
+        <h1>Oi gatinha! Sabe que eu te amo muitooo ❤️</h1>
+        <p>Grandes conquistas estão por vir, basta acreditar! Não desista dos seus sonhos (:</p>
+        <p style={{fontSize: '0.8rem'}}>Espero que essa mensagem atinga seu coração ❤️</p>
+    </div>
 }
 export default Home;
