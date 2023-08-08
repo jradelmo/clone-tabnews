898affac437b13c5514b1b4c915b38ac476a0a36
Adelmo Junior <adelmojr07@gmail.com>
2023-08-07 22:04:18 -0300

    chore: install prettier

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 function Home() {
-    return <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column'}}>
-        <h1>Oi gatinha! Sabe que eu te amo muitooo ❤️</h1>
-        <p>Grandes conquistas estão por vir, basta acreditar! Não desista dos seus sonhos (:</p>
-        <p style={{fontSize: '0.8rem'}}>Espero que essa mensagem atinga seu coração ❤️</p>
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Oi gatinha! Sabe que eu te amo muitooo ❤️</h1>
+      <p>
+        Grandes conquistas estão por vir, basta acreditar! Não desista dos seus
+        sonhos (:
+      </p>
+      <p style={{ fontSize: "0.8rem" }}>
+        Espero que essa mensagem atinga seu coração ❤️
+      </p>
     </div>
+  );
 }
 export default Home;
